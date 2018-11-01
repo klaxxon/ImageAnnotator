06db11588379a9dc5500d49e6a17eb81bd81714b
klaxxon <jon@gettys.org>
2018-11-01 17:23:27 -0500

    Fixes

--- tools/createYoloTestAndTrain.php
+++ tools/createYoloTestAndTrain.php
@@ -12,7 +12,15 @@ $batch = array();
 $d = opendir($images_dir);
 while ($f = readdir($d)) {
   if (substr($f, -4) != '.txt') continue;
-  $batch[] = $f;
+  // Verify image exists
+  $img = findImageFile($images_dir, substr($f, 0, strlen($f) - 4));
+  if ($img != null) {
+    $o = new stdClass();
+    $o->image = $img;
+    $o->fn = $f;
+    $batch[] = $o;
+  }
+  else echo "Cannot find image associated with '$f'.\n";
 }
 closedir($d);
 
@@ -26,8 +34,7 @@ echo "Creating test.txt....";
 $pfile = fopen("test.txt", "w");
 for($a=0;$a<$total*$TEST_PERCENTAGE/100.0;$a++) {
   $fn = array_pop($batch);
-  $fn = substr($fn, 0, strlen($fn) - 4);
-  fwrite($pfile, "$cwd/$images_dir/$fn\n");
+  fwrite($pfile, "$cwd/$images_dir/".$fn->image."\n");
   $cnt++;
 }
 fclose($pfile);
@@ -37,9 +44,26 @@ $cnt = 0;
 echo "Creating train.txt....";
 $pfile = fopen("train.txt", "w");
 while ($fn = array_pop($batch)) {
-  $fn = substr($fn, 0, strlen($fn) - 4);
-  fwrite($pfile, "$cwd/$images_dir/$fn\n");
+  fwrite($pfile, "$cwd/$images_dir/".$fn->image."\n");
   $cnt++;
 }
 fclose($pfile);
 echo "$cnt images\n";
+
+
+function findImageFile($dir, $pattern) {
+  $imgexts = array('.jpg','.jpeg','.png','.gif');
+  $len = strlen($pattern);
+  $d = opendir($dir);
+  while ($f = readdir($d)) {
+    if (substr($f, 0, $len) == $pattern){
+      $ext = strtolower(substr($f, $len));
+      if (in_array($ext, $imgexts)) {
+        closedir($d);
+        return $f;
+      }
+    }
+  }
+  closedir($d);
+  return null;
+}
